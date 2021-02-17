425cb8983d19e6df24efe90760bcf69407e483f3
roopesar <roope.sarasoja@metropolia.fi>
2021-02-17 12:33:50 +0200

    Testi String lisätty

--- app/src/main/java/com/example/hopapp/MainActivity.java
+++ app/src/main/java/com/example/hopapp/MainActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    String testi = "tämä on hieno testi";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
