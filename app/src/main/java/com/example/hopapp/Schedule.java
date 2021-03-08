b1c2c0abb3e6bc51b233902dca6462f097ad2d47
sanku <san.kukkonen@gmail.com>
2021-03-09 01:08:52 +0200

    pieniä muutoksia

--- app/src/main/java/com/example/hopapp/Schedule.java
+++ app/src/main/java/com/example/hopapp/Schedule.java
@@ -2,6 +2,7 @@ package com.example.hopapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -24,6 +25,7 @@ ListView lv;
         lv.setAdapter(new ArrayAdapter<Routine>(this,
                 android.R.layout.simple_list_item_1,
                 SelectedRoutinesSingleton.getInstance().getSelectedRoutines()));
+
         lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
