4db3e0b224f5a23493ea6db23344aae3d6b1f598
sanku <san.kukkonen@gmail.com>
2021-03-09 03:12:03 +0200

    mergasin wilman jutut tähän. omalta osaltani routines kommentoitu

--- app/src/main/java/com/example/hopapp/Routine.java
+++ app/src/main/java/com/example/hopapp/Routine.java
@@ -15,11 +15,11 @@ public class Routine {
         mText1 = text1;
         mText2 = text2;
     }
-
+    /**
+     * Toinen konstruktori pvm:ien lisaamista varten
+     * */
     public Routine( int imageResource, String text1, String text2, int year, int month, int dayOfMonth){
-        /**
-         * Toinen konstruktori pvm:ien lisaamista varten
-         * */
+
         mImageResource = imageResource;
         mText1 = text1;
         mText2 = text2;
@@ -40,39 +40,70 @@ public class Routine {
         return mText2;
     }
 
+    /**
+     * asettaa rutiininajankohdan vuoden
+     * @param year vuosi
+     * */
     public void setYear(int year){      // date setters
         this.year = year;
     }
 
+    /**
+     * asettaa rutiininajankohdan kuukauden
+     * @param month kuukausi
+     * */
     public void setMonth(int month){
         this.month = month;
     }
 
+    /**
+     * asettaa rutiininajankohdan paivan
+     * @param day paiva
+     * */
     public void setDayOfMonth(int day){
         this.dayOfMonth = day;
     }
 
+
+    /**
+     * palauttaa rutiininajankohdan vuoden
+     * @return int year vuosi
+     * */
     public int getYear(){       // date getters
         return this.year;
     }
 
+    /**
+     * palauttaa rutiininajankohdan kuukauden
+     * @return int month kuukausi
+     * */
     public int getMonth(){
         return this.month;
     }
-
+    /**
+     * palauttaa rutiininajankohdan paivan
+     * @return int day paiva
+     * */
     public int getDayOfMonth(){
         return this.dayOfMonth;
     }
-
+    /**
+     * palauttaa rutiininajankohdan pvm kokonaisuudessaan
+     * @return dayofMonth
+     * @return month
+     * @return year
+     * */
     public String getDateFull(){    // returns d/m/yyyy
         return dayOfMonth + ". " + month + " " + year;
     }
 
+    /**
+     * palauttaa aktiviteetin otsikon & pvm miellyttavan nakoisesti Schedule-luokan listaan
+     * @return
+     * */
     @Override
     public String toString() {
-        /**
-         * palauttaa aktiviteetin otsikon & pvm miellyttavan nakoisesti Schedule-luokan listaan
-         * */
+
         return  this.mText1 +
                 ", " + dayOfMonth +
                 ". " + (month+1) +
